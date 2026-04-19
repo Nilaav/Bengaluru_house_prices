ca82c060675759a363a871d77cbd7fa72bdf8ce5
Jyotsna S Nair <108193581+Nilaav@users.noreply.github.com>
2026-04-19 13:48:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("model.pkl")
+
+st.title("Bangalore House Price Prediction")
+
+sqft = st.number_input("Total Sqft", min_value=1.0)
+bath = st.number_input("Bathrooms", min_value=1)
+balcony = st.number_input("Balcony", min_value=0)
+
+if st.button("Predict"):
+    input_df = pd.DataFrame([[sqft, bath, balcony]],
+                            columns=['total_sqft', 'bath', 'balcony'])
+
+    prediction = model.predict(input_df)
+
+    st.success(f"Predicted Price: {prediction[0]}")
